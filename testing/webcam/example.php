f64956c0e8af3fb152b2e09075ffb7e2512a9211
Wesdewess <35997013+Wesdewess@github.com>
2019-03-15 13:18:43 +0100

    image is saved with correct name and format

--- testing/webcam/example.php
+++ testing/webcam/example.php
@@ -29,15 +29,20 @@
     capture.innerHTML = "Capture Screenshot"
     let canvas = document.getElementById("captured")
 
-
     capture.onclick = function() {
         canvas.width = video.videoWidth;
         canvas.height = video.videoHeight;
         canvas.getContext('2d').drawImage(video, 0, 0);
-        let image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
-        window.location.href=image; // it will save locally
      }
 
+
+    let link = document.createElement('a');
+    link.innerHTML = 'download snapshot';
+    link.addEventListener('click', function(ev) {
+        link.href = canvas.toDataURL();
+        link.download = "capture.png";
+    }, false);
+    document.body.appendChild(link);
 </script>
 </body>
 </html>
