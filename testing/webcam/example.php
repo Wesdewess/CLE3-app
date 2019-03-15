602c69bff1d0a5dd8084478acc80ff9c86bf28fb
Wesdewess <35997013+Wesdewess@github.com>
2019-03-15 12:40:36 +0100

    capture can be saved to disk

--- testing/webcam/example.php
+++ testing/webcam/example.php
@@ -33,7 +33,10 @@
     capture.onclick = function() {
         canvas.width = video.videoWidth;
         canvas.height = video.videoHeight;
-        canvas.getContext('2d').drawImage(video, 0, 0);}
+        canvas.getContext('2d').drawImage(video, 0, 0);
+        let image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
+        window.location.href=image; // it will save locally
+     }
 
 </script>
 </body>
