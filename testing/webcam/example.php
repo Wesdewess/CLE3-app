bb76a52e9e6a98b6e4eadc7aa4e58afd8e646708
Wesdewess <35997013+Wesdewess@github.com>
2019-03-15 14:10:11 +0100

    client side saving disabled, attempting to make server side saving work

--- testing/webcam/example.php
+++ testing/webcam/example.php
@@ -1,3 +1,27 @@
+<?php
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $img = $_POST['img'];
+    $img = str_replace('data:image/png;base64,', '', $img);
+    $img = str_replace(' ', '+', $img);
+    $data = base64_decode($img);
+    $file = 'uploads/img'.date("YmdHis").'.png';
+
+    if (file_put_contents($file, $data)) {
+        echo "<p>The canvas was saved as $file.</p>";
+    } else {
+        echo "<p>The canvas could not be saved.</p>";
+    }
+
+}
+
+
+
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,6 +33,10 @@
     <video autoplay="true" id="videoElement"></video>
     <canvas id="captured"></canvas>
     <button id="screenshot"></button>
+    <form method="post" action="" onsubmit="prepareImg();">
+        <input id="inp_img" name="img" type="hidden" value="">
+        <button id="save" type="submit" value="Upload"></button>
+    </form>
 </div>
 
 
@@ -28,14 +56,33 @@
     let capture = document.getElementById("screenshot")
     capture.innerHTML = "Capture Screenshot"
     let canvas = document.getElementById("captured")
+    let save = document.getElementById("save")
+    save.innerHTML = "save to file"
 
+    save.onclick = function() {
+        $.ajax({
+            type: "POST",
+            url: "script.php",
+            data: {
+                imgBase64: dataURL
+            }
+        }).done(function (o) {
+            console.log('saved');
+        });
+    }
     capture.onclick = function() {
         canvas.width = video.videoWidth;
         canvas.height = video.videoHeight;
         canvas.getContext('2d').drawImage(video, 0, 0);
      }
 
+    function prepareImg() {
+        let canvas = document.getElementById('canvas');
+        document.getElementById('inp_img').value = canvas.toDataURL();
+    }
 
+
+     /*
     let link = document.createElement('a');
     link.innerHTML = 'download snapshot';
     link.addEventListener('click', function(ev) {
@@ -43,6 +90,7 @@
         link.download = "capture.png";
     }, false);
     document.body.appendChild(link);
+    */
 </script>
 </body>
 </html>
